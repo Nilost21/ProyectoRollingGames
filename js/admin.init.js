e9b9135d931f90b6ae3543677cba0af9d6d4b63d
Braian Mena <braianmena@gmail.com>
2024-01-10 19:42:46 -0300

    Implementacion de 'TableRow-Juegos.js' - Declaracion y creación de archivos a utilizar en 'admin.init.js'

---
+++ js/admin.init.js
@@ -0,0 +1,4 @@
+// IMPORTAR NAVBAR
+import tableRowJuegos from "./components/TableRow-Juegos";
+import { getJuegos } from "./services/getJuegos";
+import { protectedAdminRoute } from "./routes/protectedAdmin.route";
---
+++ js/components/TableRow-Juegos.js
@@ -0,0 +1,43 @@
+
+
+
+const tableRowJuegos = (juego) =>{
+    return `
+    <tr>
+        <th scope="row">${juego.id}</th>
+        <td>${juego.name}</td>
+        <td>${juego.category}</td>
+        <td class="">${juego.description}</td>
+        <td>${juego.published ?
+            `<div class="form-check w-100 d-flex justify-content-center m-0 p-0">
+                <input type="checkbox" class="form-check-input" checked onchange="setPublished('${juego.id}')">
+            </div>` :
+            `<div class="form-check w-100 d-flex justify-content-center m-0 p-0">
+                <input type="checkbox" class="form-check-input" onchange="setPublished('${juego.id}')">
+            </div>`
+        }
+        </td>
+        <td>
+            <button type="button" class="btn btn-sm btn-secondary  me-1">Editar<i class="bi bi-pencil ms-1" data-bs-target = "#edit-juego-modal"></i></button>
+
+            <button type="button" class="btn btn-sm btn-info me-1 " data-bs-target = "#destacar-juego-modal">Destacar<i class="bi bi-star ms-1"></i></button>
+
+            <button type="button" class="btn btn-sm btn-danger me-1 mt-2" data-bs-target = "#delete-juego-modal">Eliminar<i class="bi bi-trash ms-1"></i></button>
+        </td>
+    </tr>
+    `;
+};
+
+
+const setPublished = (id) => {
+    const juegos = getJuegos();
+    const juegoIndex = getJuegoIndexById(id);
+
+    juegos[juegoIndex] = !juegos[juegoIndex].published;
+    setModifiedJuegos(juegos);
+    renderTableBody();
+};
+
+window.setPublished = setPublished;
+
+export default tableRowJuegos;
