9266ed2e5ffea8a1030c5647c1ccfea049c67e16
Braian Mena <braianmena@gmail.com>
2024-01-25 17:32:47 -0300

    Comienzo de funcionalidad de 'submit' en login - Implementacion de 'setLoggedUser.js'

--- js/login.init.js
+++ js/login.init.js
@@ -1,5 +1,6 @@
 // IMPORTAR NAVBAR
 import { showPassword } from "./utils/showPassword.js";
+import { setLoggedUser } from "./services/setLoggedUser.js";
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
@@ -13,3 +14,31 @@ const invalidLoginFeedback = document.getElementById("login_feedback_invalido");
 showPasswordBtn.addEventListener("click", (e) => {
     showPassword(e);
 });
+
+
+const feedback = (bool) => {
+    invalidLoginFeedback.classList.add("hidden");
+
+    if(bool){
+        return invalidLoginFeedback.classList.add("hidden");
+    } else {
+        return invalidLoginFeedback.classList.remove("hidden");
+    }
+}
+
+
+const userLogin = (e) => {
+    e.preventDefault();
+
+    const datosDeFormulario = Object.fromEntries(new FormData(e.target));
+
+    if(validateLoginUser({email: datosDeFormulario.email, password: datosDeFormulario.password})){
+        feedback(true);
+        setLoggedUser(datosDeFormulario.email);
+        setTimeout()
+        return;
+    }
+    return feedback(false);
+}
+
+loginForm.addEventListener("submit", userLogin);
