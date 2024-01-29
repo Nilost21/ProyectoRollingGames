7fc112582fa23dd2f5a2a8d22a2fe309fb23deea
Nilost21 <nicolasmolina0920@hotmail.com>
2024-01-29 03:06:53 -0300

    Terminando el carrousel

---
+++ js/index.init.js
@@ -0,0 +1,7 @@
+import { filtroBusqueda } from './services/filtroBusqueda.js';
+import { carruselCategoría } from './services/carruselCategoria.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  filtroBusqueda();
+  carruselCategoría();
+});
---
+++ js/services/carruselCategoria.js
@@ -0,0 +1,50 @@
+import { obtenerJuegos } from '../utils/obtenerDatos.js';
+import { mostrarCategoriaHTML } from './mostrarCategoriaHTML.js';
+export const contenedorCards = document.querySelector('#contenedor-cards');
+export const rowCards = document.querySelector('#row-cards');
+export const spanCategoria = document.querySelector('#span-categoria');
+
+export const carruselCategoría = async () => {
+  const juegos = await obtenerJuegos();
+  // Selectores
+  const btnIzq = document.querySelector('#btn-izq');
+  const btnDer = document.querySelector('#btn-der');
+
+  // Funciones
+  let categorias = ['AVENTURA', 'ACCION', 'DEPORTE']; // Lista de categorías
+  let currentIndex = 0; // Índice de la categoría actual
+
+  // Evento para cambiar de categoría al presionar el botón izquierdo
+  btnIzq.addEventListener('click', (e) => {
+    e.preventDefault();
+    currentIndex = (currentIndex - 1 + categorias.length) % categorias.length;
+    mostrarJuegosCategoria(categorias[currentIndex]);
+    actualizarCategoria(categorias[currentIndex]);
+  });
+
+  // Evento para cambiar de categoría al presionar el botón derecho
+  btnDer.addEventListener('click', (e) => {
+    e.preventDefault();
+    currentIndex = (currentIndex + 1) % categorias.length;
+    mostrarJuegosCategoria(categorias[currentIndex]);
+    actualizarCategoria(categorias[currentIndex]);
+  });
+
+  // Función para mostrar los juegos de una categoría dada
+  const mostrarJuegosCategoria = (categoria) => {
+    const juegosCategoria = juegos
+      .filter((juego) => juego.category === categoria)
+      .slice(0, 5);
+    console.log(juegosCategoria);
+    mostrarCategoriaHTML(juegosCategoria);
+  };
+
+  const actualizarCategoria = (categoria) => {
+    spanCategoria.textContent = categoria;
+
+    spanCategoria.classList.add('animacion-izquierda-derecha');
+    setTimeout(() => {
+      spanCategoria.classList.remove('animacion-izquierda-derecha');
+    }, 2000);
+  };
+};
