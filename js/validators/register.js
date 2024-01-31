af5f39ac09258d42a857012032e9bf14b347c645
Alvaro Deguer <alvarodeguer@gmail.com>
2024-01-31 19:17:24 -0300

    Agregado comportamiento cuando se trata de copiar y pegar password

--- js/validators/register.js
+++ js/validators/register.js
@@ -1,7 +1,17 @@
 import { showPassword } from "../utils/showPassword.js";
+import { validateRepeatPassword } from "./validateRepeatPassword.js";
 
 const showPasswordButton = document.getElementById("showPasswordBtn")
+const signUpFormRepeatPassword = document.getElementById("repeatSignUpPassword")
 
 showPasswordButton.addEventListener("click", (e)=>{
     showPassword(e)
 })
+
+signUpFormRepeatPassword.addEventListener('paste', (e) => {
+    e.preventDefault();
+
+    const repeatPasswordValue = signUpFormRepeatPassword.value; //obtiene lo escrito en password
+    const pastedValue = e.clipboardData.getData('text'); //obtiene lo copiado en memoria
+    const repeatedPasswordValid = validateRepeatPassword(repeatPasswordValue, pastedValue); //usa el metodo validateRepeatPassword
+});
