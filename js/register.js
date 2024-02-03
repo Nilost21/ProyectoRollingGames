8ebb9946900ba01ccafd58c5c51489b315478542
Alvaro Deguer <alvarodeguer@gmail.com>
2024-02-03 02:26:26 -0300

    Se empieza a desarrollar validacion para email y se agrega metodo para cargar usuarios en el localStorage

--- js/register.js
+++ js/register.js
@@ -1,16 +1,25 @@
 import { showPassword } from "./utils/showPassword.js";
-import { validateRepeatPassword } from "./validators/validateRepeatPassword.js";
 import { validateSignUpPassword } from "./validators/validateSignUpPassword.js";
+import { validateRepeatPassword } from "./validators/validateRepeatPassword.js";
+import { validateEmail } from "./validators/validateEmail.js";
+import { setUsuarios } from "./services/setUsuarios.js";
 
-const showPasswordButton = document.getElementById("showPasswordBtn")
-const signUpFormPasswordInput =  document.getElementById("emailSignUpPassword")
-const signUpFormRepeatPassword = document.getElementById("repeatSignUpPassword") //segundo password
-const signUpForm = document.getElementById("signUpForm")
+document.addEventListener("DOMContentLoaded", () => {
+    // NAVBAR
+    setUsuarios(); 
+    // FOOTER
+});
+
+const showPasswordButton = document.getElementById("showPasswordBtn");
+const signUpFormPasswordInput =  document.getElementById("emailSignUpPassword");
+const signUpFormRepeatPassword = document.getElementById("repeatSignUpPassword");
+const signUpForm = document.getElementById("signUpForm");
 
 //DOCUMENTAR: mostrar el password
-showPasswordButton.addEventListener("click", (e)=>{
+showPasswordButton.addEventListener("click", (e) => {
     showPassword(e)
 })
+
 
 //DOCUMENTAR: no se pega contraseña 1 y sale una alerta
 signUpFormPasswordInput.addEventListener('paste', (e) => {
@@ -32,27 +41,52 @@ signUpFormRepeatPassword.addEventListener('paste', (e) => {
  */
 
 const passwordFeedback = (password) =>{
-    signUpFormPasswordInput.classList.remove("is-valid") //limpia
+    signUpFormPasswordInput.classList.remove("is-valid") //limpia estado
     signUpFormPasswordInput.classList.remove("is-invalid")
 
     if (validateSignUpPassword(password)) {
         signUpFormPasswordInput.classList.add("is-valid")
         return true
     }
-    else{
+    else {
         signUpFormPasswordInput.classList.add("is-invalid")
         return false
     }
 }
 
+ /**
+ * 
+ * @param {string} password Recibe una contraseña
+ * @param {string} repeatPassword  Recibe otra contraseña
+ * @returns Debe mostrar el feedback de comparación de contraseñas valida o invalida segun corresponda.
+ */
 
-//test para validar contraseña valida/invalida
+const repeatPasswordFeedback = (password,repeatPassword) =>{
+    signUpFormRepeatPassword.classList.remove("is-valid")
+    signUpFormRepeatPassword.classList.remove("is-invalid")
+
+    if (validateRepeatPassword(password,repeatPassword)) {
+        signUpFormRepeatPassword.classList.add("is-valid")
+        return true
+    }
+    else{
+        signUpFormRepeatPassword.classList.add("is-invalid")
+        return false
+    }
+}
+
+
+
+
+//TEST para validar contraseñas iguales
 const signUpSubmit = (e) => {
     e.preventDefault();
 
     const testPassword = document.getElementById("emailSignUpPassword").value
+    const testPassword2 = document.getElementById("repeatSignUpPassword").value
 
-    passwordFeedback(testPassword);
+    repeatPasswordFeedback(testPassword,testPassword2)
+    
 }
 
 signUpForm.addEventListener("submit", signUpSubmit);
