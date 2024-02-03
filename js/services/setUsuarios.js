e6429e9d7aff1acebe9fb76de92e328fbcf650d8
Alvaro Deguer <alvarodeguer@gmail.com>
2024-02-03 02:24:16 -0300

    Documentada funcion setUsuarios.js

--- js/services/setUsuarios.js
+++ js/services/setUsuarios.js
@@ -1,5 +1,9 @@
 import usuariosDB  from "../database/usuarios.json" assert {type: "json"};
 
+/**
+ * 
+ * Carga los datos de un archivo Json en el localStorage
+ */
 export const setUsuarios = () => {
     const usuarios = JSON.parse(localStorage.getItem("usuarios")) || null;
     if(!usuarios || usuarios?.length == 0){
